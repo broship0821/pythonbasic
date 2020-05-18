b9f162a342cc2ace0210bd917832285b672eced2
gudqo1995 <gudqo1995@naver.com>
2020-05-18 11:52:52 +0900

    print, ;

---
+++ class_0518.py
@@ -0,0 +1,16 @@
+print("오늘은 파이썬 시작~~^^")
+
+#c언어
+#include<stdio.h>
+#int main (void)
+#{
+#    printf("오늘은 파이썬 시작\n");
+#    return 0;
+#}
+
+print("hello") #파이썬은 들여쓰기로 종속을 구분
+print("python 첫날입니다")
+print("python을 시작해 볼까요")
+
+print("쌍다옴표 안의 내용 출력"); print("이어 쓰기 실행")#한 줄에 2오더를 입력하려면 ";"입력, 가독성은 떨어짐, 연관성이 있거나 짧은 문장만 사용
+print("이어쓰기;");print();print(";(쎄미콜론이라고 읽는다)")
