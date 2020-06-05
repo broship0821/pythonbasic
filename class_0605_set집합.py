cb6efb13f3d7de8d7177c0d141001172e581cff2
gudqo1995 <gudqo1995@naver.com>
2020-06-05 11:43:05 +0900

    set집합, 문자열 + ppt자료

---
+++ class_0605_set집합.py
@@ -0,0 +1,88 @@
+# set 집합
+# list-index/dic-key로 첨자가 있었는데 set은 첨자가 없음 - 중복불가
+# 저장속도는 빠르나 검색은 반복하여 찾아야됨 / 자료 수집에 몰빵
+# 집합은 여러 값들의 모임, 저장순서 보장안되고 중복값을 허용하지않음
+# 딕셔너리처럼 {}로 쓰나 key,value값이 지정되어있지 않음
+
+#print할때마다 순서가 바뀜
+# names = {'허준', '신사임당', '권율', '홍길동', '허준                 ', 212, 'ㅅㅅㅅ'}
+# print(type(names)) #class 'set'
+# print(len(names)) #집합의 개수 (4, 중복제거)
+# print(names) #중복되어 있는 값은 있어도 오류는 나지 않지만 하나만 표시
+
+# s = {}
+# print(type(s)) #그냥 {}면 dic으로 인지함
+# s = set({}) #set 자료형 초기화
+# print(type(s))
+# #순서없이 중복제거하여 출력됨
+# print(set('progr               ammmmmming')) #공백도 하나의 문자로 인지
+# print(set('asdfdasf''qwerqwer')) # 이렇게 하나의 문자열이면 가능, ,로 2개로 구분지으면 에러남
+# print(set([12,14,16,25,11,15, 12]))
+# dic = {'a':1, 'b':2, 'c':3}
+# print(set(dic)) # key값만 set으로 가져옴
+
+# 순서 전혀 없음
+# for i in {'가','나','다','라'}:
+#     print(i)
+
+### set 조작함수
+## .add(추가할 값): 순서없이 추가되기 때문에 .append랑 다름
+# asia = {'korea', 'china', 'japan'}
+# print(asia)
+# asia.add('north_korea')
+# print(asia)
+# asia.add('china') #이미 있는 값은 추가 안됨, 공백이나 _를 써서 다른값을 만들어줘야됨
+# print(asia)
+# ## .remove(삭제할 값): 인덱스가 없기 때문에 .del()은 사용할수없음
+# ## .del(삭제할 인덱스 위치), .remove(삭제할 값)
+# asia.remove('japan')
+# print(asia)
+
+## .update(추가할 집합)
+# asia = {'korea', 'china', 'japan'}
+# print(asia)
+# asia2 = {'iraq', 'singapore', 'korea'}
+# asia.update(asia2)
+# print(asia)
+# print("-"*40)
+# asia = {'korea', 'china', 'japan'}
+# asia2 = {'iraq', 'singapore', 'korea'}
+# #asia3 = asia + asia2 # +연산 사용 불가능
+# #print(asia3)
+
+# set 집합의 연산
+# 1. 합집합(|): 두 집합의 전체 요소들의 모음
+# 2. 교집합(&): 두 집합의 공통 요소들의 모음
+# 3. 차집합(-): 왼쪽 집합의 요소에서 오른쪽 집합의 요소를 제거
+# 4. 배타적 차집합(^): 합집합 - 교집합
+# 5. 부분집합(<=): 왼쪽 집합이 오른쪽 집합의 부분집합인지의 여부를 확인
+# 6. 진성 부분집합(<): 부분집합이면서 추가로 요소가 더 존재하는지 확인
+# 부분집합과 진성부분집합의 차이는:
+#     부분집합(<=) 경우는 좌우 집합이 같아도 부분집합이다
+#     진성부분집합(<)은 좌우 집합이 모두 같을 경우 성립되지 않는다
+# asia = {'korea', 'china', 'japan'}
+# asia2 = {'iraq', 'singapore', 'korea'}
+# print(asia|asia2)
+# print(asia&asia2)
+# print(asia-asia2)
+# print(asia^asia2)
+# asia3 = {'korea', 'china', 'japan', 'north_korea'}
+# print(asia<=asia3)
+# print(asia<asia3)
+# asia4 = {'korea', 'china', 'japan', 'north_korea'}
+# print(asia4<=asia3)
+# print(asia4<asia3)
+
+# two = {2,4,6,8,10,12}
+# three = {3,6,9,12,15}
+# print("교집합:", two & three) #6 12
+# print("합집합:", two | three) #2,3,4,6,8,9,10,12,15
+# print("차집합:", two - three) #2,4,8,10
+# print("배타적 차집합:", two ^ three) #2,3,4,8,9,10,15
+
+# animal = {'호랑이','사자','강아지','치타','햄스터','고양이'}
+# pet = {'강아지','고양이','햄스터'}
+# print(pet<=animal)
+# print(pet<=pet)
+# print(pet<animal)
+# print(pet<pet)
---
+++ class_0605_문자열.py
@@ -0,0 +1,105 @@
+## 문자열
+# Str = 'Have a nice day'
+# for i in range(len(Str)):
+#     print("str[%d]:"%i, Str[i])
+# print()
+# for i in range(len(Str)): #인덱스 이용하기
+#     print(Str[i], end="")
+# print()
+# for i in Str: #한꺼풀 벗겨내기
+#     print(i, end="")
+# print()
+# print("문자열의 총 길이:", len(Str))
+# print("Str:", Str)
+
+
+# Str = 'Have a nice day'
+# arr = Str[7:11]
+# print(Str)
+# print(arr)
+
+# Str = "즐거운 파이썬"
+# print(Str)
+# print(Str[0])
+# print(Str[1:5])
+# print(Str[3:])
+# print(Str[:3])
+
+# Str = "Have a"
+# print(Str)
+# print("_"*64)
+# Str += " nice day"
+# print(Str)
+# print(Str*3)
+
+#### 문자열 함수
+## .upper():소문자를 대문자로 변경/ .lower():대문자를 소문자로 변경
+## .swapcase(): 대소문자 상호 변경/ .title():각 단어의 첫번째 문자 대문자로 변경
+# Str = 'Python is Easy. 그리고 programming 할만하다 ^^123'
+# print(Str)
+# print(Str.upper())
+# print()
+# print(Str.lower())
+# print()
+# print(Str.swapcase())
+# print()
+# print(Str.title()) # 각 단어의 첫번째 문자 대문자로 변경
+# print()
+
+#.title()은 첫글자를 대문자로 만들어줄뿐 아니라 나머지 글자는 소문자로 만들어줌
+# 첫글자가 숫자나 특수기호, 한글일 경우 무시하고 영어 부분만 바꿔줌
+# st = 'NevEr -eVer 110gIVe 업up'
+# st=st.title()
+# print(st)
+
+
+# ## .count('찾는 문자or문자열')
+# st = "Have a nice day"
+# print(st)
+# print()
+# print("'a'개수:", st.count('a'))
+# print("' '개수:", st.count(' '))
+# print(st.count('')) #아무것도 안적으면 총 문자열 개수 알려줌/문자열은 끝을 알려주는null문자가 있기때문에 문자열개수+1된 값이 나옴
+# print("'da'개수:", st.count('da'))
+# print("'dad'개수:", st.count('dad'))
+
+# st = "It is a fun Python class"
+# a, s, All = 0, 0, 0
+# for i in st:
+#     All += 1
+#     if i == 'a':
+#         a += 1
+#     elif i == 's':
+#         s += 1
+# print("총개수:", All)
+# print("a 개수:", a)
+# print("s 개수:", s)
+# ## 이렇게 바로 변환할 수 있음
+# print("총개수:", st.count("")-1)
+# print("총개수:", len(st))
+# print("a 개수:", st.count("a"))
+# print("s 개수:", st.count("s"))
+
+
+# ## .find('찾을 문자or문자열'): 첫글자의 위치 인덱스 반환/못찾으면 -1반환 .index()도 똑같으나 못찾으면 에러남
+# st = "Have a nice day"
+# print(st)
+# print("find: 'day'위치:", st.find('day')) # 시작 인덱스의 위치값 출력
+# print("index: 'day'위치:", st.index('day'))
+# print("find: 'kkk'위치:", st.find('kkk'))
+# # print("index: 'kkk'위치:", st.index('kkk'))
+
+# st = "Have a nice day"
+# print(st)
+# print("find: 'a'위치:", st.find('a')) #첫번째께 찾아짐
+# print("find: 'a'위치:", st.find('a',2)) # 2번 인덱스 이후부터 찾아줘
+# print("find: 'a'위치:", st.find('a',6)) # 6번 인덱스 이후부터 찾아줘
+# print("find: 'a'위치:", st.find('a',14))
+
+# a의 총 개수와 첨자의 위치를 출력 하시오
+st = "Have a nice day Have a nice day Have a nice day"
+# 결과: [1,5,13,17,21,29,33,37,45]
+# while .find()로 -1 이 될때까지
+ls = []
+while 
+print(st.find('a'))
